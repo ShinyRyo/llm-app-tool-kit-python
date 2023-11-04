e6d10e63282d965fddad641ea971c37bd3decd6e
onikarubi <printuserselect@gmail.com>
2023-11-04 17:47:02 +0900

    Development Environment Settings

---
+++ src/fine_tuning/data_format_generator.py
@@ -0,0 +1,47 @@
+import openai
+import os
+import tiktoken
+import json
+import csv
+
+class ChatTemplate:
+    def __init__(self, csv_path) -> None:
+        self.csv_path = csv_path
+        self._templates = self._read_chat_templates()
+
+    def _read_chat_templates(self) -> list[dict[str, str]]:
+        template_list = []
+        with open(self.csv_path) as csv_file:
+            templates = csv.DictReader(csv_file)
+
+            return [template for template in templates]
+
+    @property
+    def templates(self) -> list[dict[str, str]]:
+        if len(self._templates) < 1:
+            raise IndexError('templatesの中身が空です')
+
+        return self._templates
+
+
+def create_training_data(csv_path):
+    chat_template = ChatTemplate(csv_path)
+    data = []
+
+    for i in range(1, len(chat_template.templates), 2):
+        data.append({'messages': [
+            chat_template.templates[0],
+            chat_template.templates[i],
+            chat_template.templates[i + 1]
+        ]})
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    json_file_path = os.path.join(current_dir, 'train_json/train.jsonl')
+
+    with open(json_file_path, 'w', encoding='utf-8') as f:
+        for item in data:
+            json_line = json.dumps(item, ensure_ascii=False)
+            f.write(json_line + '\n')
+
+
+
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from fine_tuning.data_format_generator import create_training_data
+
+if __name__ == '__main__':
+    create_training_data('src/fine_tuning/train-txt/yukkuri-marisa.txt')
